b133d57b6d426471ea9bf6ef3d667135d009b0eb
soonman <soooon1001@gmail.com>
2019-08-06 23:19:37 +0900

    서버구축
    Co-authored-by: Taesu Hyeon <email@qvil.dev>

---
+++ select_area.php
@@ -0,0 +1,8 @@
+<?php 
+// The global $_POST variable allow to access the data send with the POST method
+  // To access the data send with the GET method, you can use $_GET
+  // $post_code = $_POST['post_code'];
+
+
+  echo $_POST['post_code'];
+  // echo 1;
