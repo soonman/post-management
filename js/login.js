acd478d27555e5de2123470964e430445306782d
soonman <soooon1001@gmail.com>
2019-08-21 23:10:49 +0900

    20190821class

---
+++ js/login.js
@@ -0,0 +1,11 @@
+console.log("load!");
+
+console.log(document.querySelector(".test1234"));
+
+function a() {}
+
+document.querySelector(".login_button").addEventListener("click", function(e) {
+  e.preventDefault();
+
+  alert("hi!");
+});
