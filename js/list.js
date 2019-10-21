fdbd7cc1c62c995600a254c80b86937f6c2f4ece
soonman <soooon1001@gmail.com>
2019-10-21 22:44:10 +0900

    로그인 안했을 때 list.html 못들어오게하기
    if절을 이용하여 쿠키가 있을때는 로그를 찍고, else면 login.html로 강제이동

--- js/list.js
+++ js/list.js
@@ -1,2 +1,16 @@
-// console.log(Cookies.get("email"));
-// document.write(Cookies.get("email"));
+console.log(Cookies.get("who"));
+document.write(Cookies.get("who"));
+
+document.querySelector(".logout_button").addEventListener("click", function(e) {
+  e.preventDefault();
+  Cookies.remove("who");
+  alert("Cookie삭제, 로그아웃 되었습니다");
+  console.log(Cookies.remove("who"));
+  window.location.href = "./login.html";
+});
+
+if (Cookies.get("who")) {
+  console.log(Cookies.get("who"));
+} else {
+  window.location.href = "./login.html";
+}
