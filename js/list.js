25f1c8453f17f72aeb851a188a8566def5416682
soonman <soooon1001@gmail.com>
2019-10-22 23:06:51 +0900

    DB베이스 환경설정, 데이터 추가

--- js/list.js
+++ js/list.js
@@ -14,3 +14,25 @@ if (Cookies.get("who")) {
 } else {
   window.location.href = "./login.html";
 }
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId
+});
+
+var db = firebase.firestore();
+
+db.collection("users")
+  .add({
+    first: "ㅎㅎ",
+    last: "ㅋㅋ",
+    born: 1990
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
