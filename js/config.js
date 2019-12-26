3e70a7d9e77c9003d62ef4b6584e6b5d39a9b75e
soonman <soooon1001@gmail.com>
2019-12-26 22:51:31 +0900

    config.js gitignore 해제

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBG3GhczYhfG0AqzAWWPK-fa2oWP_dKYvo",
+    authDomain: "post-management-446e5.firebaseapp.com",
+    databaseURL: "https://post-management-446e5.firebaseio.com",
+    projectId: "post-management-446e5",
+    storageBucket: "post-management-446e5.appspot.com",
+    messagingSenderId: "74266025631",
+    appId: "1:74266025631:web:3ab66d352c17e35b"
+  };
